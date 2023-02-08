bfe168da6dde309a9312023f40589057669b652e
Uthaeus <romanlavery@gmail.com>
2023-02-07 20:57:42 -0700

    checking for overall form validity and slimming code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -8,6 +8,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
   };
@@ -44,7 +50,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
