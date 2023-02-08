f71c81e11d2cadeca92b83f43f8b20d1ca72eee2
Uthaeus <romanlavery@gmail.com>
2023-02-07 20:46:27 -0700

    implemented simple validity and consolidated duplicated code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,35 +1,33 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
   };
+
+  const nameInputBlurHandler = (e) => {
+    setEnteredNameTouched(true);
+  }
 
   const formSubmissionHandler = e => {
     e.preventDefault();
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-
-    console.log(enteredName);
-    // const enteredValue = nameInputRef.current.value;
-
     setEnteredName('');
+    setEnteredNameTouched(false);
   };
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
@@ -37,7 +35,12 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+        <input 
+          type='text' id='name' 
+          onChange={nameInputChangeHandler} 
+          onBlur={nameInputBlurHandler}
+          value={enteredName} 
+        />
         {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
